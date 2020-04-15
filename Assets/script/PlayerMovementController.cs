7356bcfad36123fe3c17d0e014d8f124b2a490c8
SakuraSnow <953260653@qq.com>
2020-04-15 18:27:50 +0800

    初始版本提交

---
+++ Assets/script/PlayerMovementController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D coll;
+    private Animator anim;
+
+    public float speed;
+    public float jumpforce;
+    public Transform groundcheck;
+    public LayerMask ground;
+
+    public bool isGround;
+    public bool isJump;
+
+    bool jumpPressed;
+    int jumpCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/script/playerController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D rb;
+    [SerializeField]private Animator anim;
+    public float speed;
+    public float jumpforce;
+    public LayerMask ground;
+    public Collider2D coll;
+    public int cherry=0;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() 
+    {
+        Movement();
+        SwitchAnimation();
+    }
+
+    void Movement()//人物移动函数
+    {
+        float horizontalmove = Input.GetAxis("Horizontal");
+        float facedirection = Input.GetAxisRaw("Horizontal");
+        //水平上给刚体赋予移动力
+        rb.velocity = new Vector2(horizontalmove * speed * Time.deltaTime, rb.velocity.y);
+        //人物移动
+        if(facedirection != 0)
+        {
+            transform.localScale = new Vector3(facedirection, 1, 1);
+        }
+        //人物移动动画
+        anim.SetFloat("running",Mathf.Abs(facedirection));
+        //人物跳跃
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce * Time.deltaTime);
+            anim.SetBool("jumping",true);
+        }
+        
+    }
+
+    void SwitchAnimation()//跳跃变换函数
+    {
+        if(anim.GetBool("jumping"))
+        {
+            if(rb.velocity.y<0)
+            {
+                anim.SetBool("jumping",false);
+                anim.SetBool("falling",true);
+            }
+        }
+        else if(coll.IsTouchingLayers(ground))//触碰地面检测
+        {
+            anim.SetBool("falling",false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //收集品触碰检测
+    {
+        if(other.tag == "collection")
+        {
+            Destroy(other.gameObject);
+            cherry++;
+        }
+    }
+}
